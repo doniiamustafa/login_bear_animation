8b7ead3e5f9e71ecad6af0575c4a56ceb3eb5fb3
doniiamustafa <doniamoustafa.dm@gmailcom>
2023-02-01 17:34:08 +0200

    new on boarding screen

--- lib/using_stream_controllers/view/on_boarding_screen.dart
+++ lib/using_stream_controllers/view/on_boarding_screen.dart
@@ -2,64 +2,151 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:login_animation/login_screen.dart';
+import 'package:login_animation/using_stream_controllers/view/login_view_stream_controllers.dart';
+import 'package:login_animation/using_stream_controllers/view/register_view.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: 700,
-        width: double.infinity,
-        child: OnBoardingSlider(
-          middle: Center(),
-          imageHorizontalOffset: -10,
-          imageVerticalOffset: -100,
+    return SafeArea(
+      child: Scaffold(
+        body: OnBoardingSlider(
           headerBackgroundColor: Colors.white,
-          finishButtonText: 'Register',
-          skipTextButton: Text('Skip'),
-          trailing: Text('Login'),
+          finishButtonText: 'Login',
+          onFinish: () {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const LoginScreen()));
+          },
+          skipTextButton: Text("Skip"),
+
+          skipFunctionOverride: () {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const LoginScreen()));
+          },
+          // trailing: Text('Skip'),
           background: [
-            Image.asset('assets/boarding.jpg'),
-            Image.asset('assets/boarding2.png'),
-            Image.asset(
-              'assets/boarding3.png',
+            SizedBox(
+                height: 400,
+                width: 400,
+                child: Image.asset('assets/boarding.jpg')),
+            SizedBox(
+                height: 400,
+                width: 350,
+                child: Image.asset('assets/boarding2.png')),
+            SizedBox(
+              height: 400,
+              width: 350,
+              child: Image.asset(
+                'assets/boarding3.png',
+              ),
             ),
           ],
           totalPage: 3,
           speed: 1.8,
           pageBodies: [
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: const <Widget>[
                   SizedBox(
-                    height: 100,
+                    height: 400,
                   ),
-                  Text('Description Text 1'),
+                  Text(
+                    'خدماتنا الطبيه',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    ' تستطيع القيام بالاستشاره من داخل التطبيق الخاص بنا من مكانك',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black38,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ],
               ),
             ),
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
+              padding: EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: const <Widget>[
                   SizedBox(
-                    height: 480,
+                    height: 400,
                   ),
-                  Text('Description Text 2'),
+                  Text(
+                    'الكشف عن المرض',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'تستطيع الكشف عن المرض عن طريق الأستعلام عن الأعراض',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black38,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ],
               ),
             ),
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
+              padding: EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: const <Widget>[
                   SizedBox(
-                    height: 480,
+                    height: 400,
                   ),
-                  Text('Description Text 3'),
+                  Text(
+                    'الثقه و الجوده',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'كل البيانات تكون مراجعه عن طريق أفضل الأطباء',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black38,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ],
               ),
             ),
